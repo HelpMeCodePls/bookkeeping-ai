8a7324323705ee933f1fa86abd649dc1ee2305ad
HelpMeCodePls <64028614+HelpMeCodePls@users.noreply.github.com>
2025-04-20 06:46:04 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    build: {
+    target: 'es2020' // 或更高的 ES 版本
+  }
   // server: {
   //   proxy: {
   //     '/ledgers': 'http://localhost', 
