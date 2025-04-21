4fb6756840ac6b2a0c392759aa332dd9c5819b29
yuxing zhang <132979037+zyuxing0717@users.noreply.github.com>
2025-04-21 15:35:13 -0600

    changes

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify, send_from_directory
 import os
 from dotenv import load_dotenv
-from backend.agents import Customer_Service_Agent  # 你定义的智能体
-from backend.agents import Analyst_Agent, Database_Agent #for  future Debug
+from backend.Agents import Customer_Service_Agent  # 你定义的智能体
+from backend.Agents import Analyst_Agent, Database_Agent #for  future Debug
 from semantic_kernel.functions import KernelArguments
 from flask_cors import CORS
 
